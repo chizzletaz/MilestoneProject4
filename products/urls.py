19fd2544153638be2d8528e51ce959a19c19259e
Tasco Silva <tascosilva@tasco.home>
2021-09-06 16:27:58 +0200

    Add product details functionality.

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('', views.all_trips, name='trips')
+    path('<product_id>', views.product_detail, name='product_detail'),
+    path('', views.all_trips, name='trips'),
 ]
