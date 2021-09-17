23dc73078b53d34216bc68a6e2e4cda06ad97936
Tasco Silva <tascosilva@tasco.home>
2021-09-17 17:39:37 +0200

    Add profile form, views and update template. Adjust profile.css to match the overall layout.

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,12 @@
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#b8bac0');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#b8bac0');
+    } else {
+        $(this).css('color', '#f1f1f1');
+    }
+});
