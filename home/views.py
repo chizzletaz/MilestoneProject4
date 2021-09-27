d326db4c108926deb97e4eb7ef73c591b9c3b4ae
Tasco Silva <tascosilva@tasco.home>
2021-09-27 14:36:51 +0200

    Update shop-section on index.html. Add Stock to product_detail page.

--- home/views.py
+++ home/views.py
@@ -11,17 +11,3 @@ def index(request):
       }
     return render(request, 'home/index.html', context)
 
-
-# class Trips: zoals webhookhandler
-#    def __init__(self, product)
-
-#   def get_trip(self, request):
-#       """ A view to return the index page """
-
-#       products = Product.objects.all().filter(category__name='trip')
-
-#       context = {
-#         'products': products,
-#       }
-
-#       return render(request, 'home/index.html', context)
