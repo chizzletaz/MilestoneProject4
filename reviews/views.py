96e186c31794c1291a4eeea4425feb9f7221d287
Tasco Silva <tascosilva@tasco.home>
2021-10-03 09:52:54 +0200

    Trying to fix add_review view.

--- reviews/views.py
+++ reviews/views.py
@@ -18,12 +18,15 @@ def add_review(request, product_id):
             product = get_object_or_404(Product, pk=product_id)
             user = UserProfile.objects.get(user=request.user)
             review_form = ReviewForm(request.POST)
+            print(review_form)
             # check if user already added a review
             added_review = Review.objects.filter(user=user, product=product)
             if added_review.exists():
+                
                 messages.error(request, 'You already reviewed this product. To change your review click the edit button on your review.')
             else:
                 if review_form.is_valid():
+                    print('1 test')
                     review = review_form.save(commit=False)
                     review.product = product
                     review.user = user
@@ -34,22 +37,23 @@ def add_review(request, product_id):
                     average_rating = reviews.aggregate(Avg('rating'))['rating__avg']
                     product.rating = average_rating
                     product.save()
-                    
+    
                     messages.success(request, 'Review succesfully added!')
-                    return redirect(reverse('product_detail', args=[product.id]))
                 else:
+                    print('This works')
                     messages.error(request, 'Failed to add review. Please ensure the form is valid.')
+            return redirect(reverse('product_detail', args=[product.id]))
+        
+        # else:
+        #     review_form = ReviewForm()
 
-        else:
-            review_form = ReviewForm()
+        # template = 'products/product_details.html'
+        # context = {
+        #     'form': review_form,
+        #     'on_profile_page': True,
+        # }
 
-        template = 'products/product_details.html'
-        context = {
-            'form': review_form,
-            'on_profile_page': True,
-        }
-
-        return render(request, template, context)
+        # return render(request, template, context)
 
     else:
         messages.error(request, 'Sorry, only logged in users can leave a review.')
--- space_travel_agency/urls.py
+++ space_travel_agency/urls.py
@@ -23,8 +23,10 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    # path('products/', include('reviews.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
+    path('reviews/', include('reviews.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
