64504cf987e4e9a3aa0e38b25b4847b1df22dbbc
Tasco Silva <tascosilva@tasco.home>
2021-10-18 13:06:49 +0200

    Remove overflow-x. Adjust margin navlinks mobile view.

--- space_travel_agency/urls.py
+++ space_travel_agency/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-    # path('products/', include('reviews.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
